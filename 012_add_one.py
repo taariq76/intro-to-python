733302a7d7b696dc70a4ca2a41d178d2020d7587
taariq76 <105609460+taariq76@users.noreply.github.com>
2025-01-24 22:35:23 +0000

    Update 012_add_one.py

--- 012_add_one.py
+++ 012_add_one.py
@@ -21,5 +21,5 @@ print(add_one(6))
 # `add_one` is a function (little machine) that takes a
 # number as an input parameter, adds one to it, and then
 # returns the result.
-
+#Done
 # Now move on to 013_add_two.py
